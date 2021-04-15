27e245426c61b6e53ad7bed717ebd536c2390797
John Topley <john.topley@ons.gov.uk>
2021-04-15 11:00:58 +0100

    Tidied up JavaScript

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,15 @@
 $(function() {
-  $("#search").on("keyup", function(event) {
-    var filter, table, tr, td, i, txtValue;
-    filter = document.getElementById("search").value.toUpperCase();
-    table = document.getElementById("data");
-    tr = table.getElementsByTagName("tr");
+  $("#filter").on("keyup", function(event) {
+    var filter, tr, td, i, cellText;
+    filter = $("#filter").val().toUpperCase();
+    tr = $("#data").find("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
       if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        cellText = td.textContent || td.innerText;
+        if (cellText.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
