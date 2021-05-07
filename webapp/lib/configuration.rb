8ab5e01df24c6a3053415ed4828191ce14a2bb90
John Topley <john.topley@ons.gov.uk>
2021-05-07 16:55:40 +0100

    Added support for Firestore project environment variable

--- webapp/lib/configuration.rb
+++ webapp/lib/configuration.rb
@@ -2,17 +2,20 @@
 
 # Simple class to centralise access to configuration.
 class Configuration
-  attr_reader :github_api_base_uri,
+  attr_reader :firestore_project,
+              :github_api_base_uri,
               :github_enterprise,
               :github_organisation,
               :github_token
 
   def initialize(env)
+    @firestore_project   = env['FIRESTORE_PROJECT']
     @github_api_base_uri = env['GITHUB_API_BASE_URI']
     @github_enterprise   = env['GITHUB_ENTERPRISE_NAME']
     @github_organisation = env['GITHUB_ORGANISATION_NAME']
     @github_token        = env['GITHUB_TOKEN']
 
+    raise 'Missing FIRESTORE_PROJECT environment variable' unless @firestore_project
     raise 'Missing GITHUB_API_BASE_URI environment variable' unless @github_api_base_uri
     raise 'Missing GITHUB_ENTERPRISE_NAME environment variable' unless @github_enterprise
     raise 'Missing GITHUB_ORGANISATION_NAME environment variable' unless @github_organisation
