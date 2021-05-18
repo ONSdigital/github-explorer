e52505d55e6afd7ac38b1b74a5d65a8518f7b1a7
John Topley <john.topley@ons.gov.uk>
2021-05-18 09:54:40 +0100

    Enabled state saving for table column sorting and searches and disabled column sorting for some columns

--- webapp/public/javascripts/app.js
+++ webapp/public/javascripts/app.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
-  $("#data").DataTable({
+  $("#data.teams").DataTable({
+    stateSave: true,
 
     // See https://datatables.net/reference/option/dom
-    "dom": "frtp" 
+    "dom": "frtp"
+  });
+
+  $("#data.members").DataTable({
+    stateSave: true,
+    "dom": "frtp"
+  });
+
+  $("#data.collaborators").DataTable({
+    stateSave: true,
+    "dom": "frtp"
+  });
+
+  $("#data.repositories").DataTable({
+
+    // Prevent sorting the Branch Protection Rules column.
+    columns: [
+      null,
+      null,
+      null,
+      { orderable: false },
+      null,
+      null,
+      null,
+      null
+    ],
+
+    stateSave: true,
+    "dom": "frtp"
+  });
+
+  $("#data.two-factor").DataTable({
+    stateSave: true,
+    "dom": "frtp"
   });
 });
