73ad70401e7f19b886ff7dd7db5aed54e7b24186
John Topley <john.topley@ons.gov.uk>
2021-06-07 15:42:38 +0100

    Added new query for contribution counts

--- agent/lib/github.rb
+++ agent/lib/github.rb
@@ -13,6 +13,37 @@ class GitHub
   CLIENT = GraphQL::Client.new(schema: SCHEMA, execute: ContextTransport.new)
   PAUSE  = 0.5
 
+  ALL_MEMBERS_CONTRIBUTIONS_QUERY = CLIENT.parse <<-'GRAPHQL'
+    query ($slug: String!, $first: Int!, $after: String) {
+      enterprise(slug: $slug) {
+        members(first: $first, after: $after) {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          nodes {
+            ... on EnterpriseUserAccount {
+              user {
+                avatarUrl
+                createdAt
+                login
+                name
+                updatedAt
+                contributionsCollection {
+                  hasAnyContributions
+                  restrictedContributionsCount
+                  totalCommitContributions
+                  totalIssueContributions
+                  totalPullRequestContributions
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
+
   ALL_MEMBERS_WITH_ROLES_QUERY = CLIENT.parse <<-'GRAPHQL'
     query($login: String!, $first: Int!, $after: String) {
       organization(login: $login) {
@@ -27,6 +58,35 @@ class GitHub
               name
             }
             role
+          }
+        }
+      }
+    }
+  GRAPHQL
+
+  ALL_OUTSIDE_COLLABORATORS_CONTRIBUTIONS_QUERY = CLIENT.parse <<-'GRAPHQL'
+    query ($slug: String!, $first: Int!, $after: String) {
+      enterprise(slug: $slug) {
+        ownerInfo {
+          outsideCollaborators(first: $first, after: $after) {
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            nodes {
+              avatarUrl
+              createdAt
+              login
+              name
+              updatedAt
+              contributionsCollection {
+                hasAnyContributions
+                restrictedContributionsCount
+                totalCommitContributions
+                totalIssueContributions
+                totalPullRequestContributions
+              }
+            }
           }
         }
       }
@@ -127,25 +187,6 @@ class GitHub
     @token        = token
   end
 
-  def all_repositories
-    after = nil
-    next_page = true
-    all_repositories = []
-
-    while next_page
-      repositories = CLIENT.query(ALL_REPOSITORIES_QUERY, variables: { login: @organisation, first: 100, after: after },
-                                                          context: { base_uri: @base_uri, token: @token })
-      raise GitHubError, repositories.errors unless repositories.errors.empty?
-
-      after = repositories.data.organization.repositories.page_info.end_cursor
-      next_page = repositories.data.organization.repositories.page_info.has_next_page
-
-      repositories.data.organization.repositories.nodes.each { |repository| all_repositories << repository }
-    end
-
-    all_repositories
-  end
-
   def all_members_teams
     after = nil
     next_page = true
@@ -209,6 +250,25 @@ class GitHub
     all_owners.sort_by(&:login)
   end
 
+  def all_repositories
+    after = nil
+    next_page = true
+    all_repositories = []
+
+    while next_page
+      repositories = CLIENT.query(ALL_REPOSITORIES_QUERY, variables: { login: @organisation, first: 100, after: after },
+                                                          context: { base_uri: @base_uri, token: @token })
+      raise GitHubError, repositories.errors unless repositories.errors.empty?
+
+      after = repositories.data.organization.repositories.page_info.end_cursor
+      next_page = repositories.data.organization.repositories.page_info.has_next_page
+
+      repositories.data.organization.repositories.nodes.each { |repository| all_repositories << repository }
+    end
+
+    all_repositories
+  end
+
   def all_two_factor_disabled
     after = nil
     next_page = true
@@ -228,6 +288,74 @@ class GitHub
     end
 
     all_two_factor_disabled.sort
+  end
+
+  def all_users_contributions
+    after = nil
+    next_page = true
+    all_users_contributions = []
+
+    while next_page
+      members_contributions = CLIENT.query(ALL_MEMBERS_CONTRIBUTIONS_QUERY, variables: { slug: @enterprise,
+                                                                                         first: 10, after: after },
+                                                                            context: { base_uri: @base_uri,
+                                                                                       token: @token })
+      raise GitHubError, members_contributions.errors unless members_contributions.errors.empty?
+
+      after = members_contributions.data.enterprise.members.page_info.end_cursor
+      next_page = members_contributions.data.enterprise.members.page_info.has_next_page
+
+      members_contributions.data.enterprise.members.nodes.each do |member|
+        user_tuple = OpenStruct.new
+        user_tuple.avatar_url                 = member.user.avatar_url
+        user_tuple.created_at                 = member.user.created_at
+        user_tuple.login                      = member.user.login
+        user_tuple.name                       = member.user.name
+        user_tuple.updated_at                 = member.user.updated_at
+        user_tuple.has_contributions          = member.user.contributions_collection.has_any_contributions
+        user_tuple.restricted_contributions   = member.user.contributions_collection.restricted_contributions_count
+        user_tuple.commit_contributions       = member.user.contributions_collection.total_commit_contributions
+        user_tuple.issue_contributions        = member.user.contributions_collection.total_issue_contributions
+        user_tuple.pull_request_contributions = member.user.contributions_collection.total_pull_request_contributions
+        user_tuple.member                     = true
+        all_users_contributions << user_tuple
+      end
+
+      sleep PAUSE
+    end
+
+    after = nil
+    next_page = true
+
+    while next_page
+      collaborators_contributions = CLIENT.query(ALL_OUTSIDE_COLLABORATORS_CONTRIBUTIONS_QUERY,
+                                                 variables: { slug: @enterprise, first: 10, after: after },
+                                                 context: { base_uri: @base_uri, token: @token })
+      raise GitHubError, collaborators_contributions.errors unless collaborators_contributions.errors.empty?
+
+      after = collaborators_contributions.data.enterprise.owner_info.outside_collaborators.page_info.end_cursor
+      next_page = collaborators_contributions.data.enterprise.owner_info.outside_collaborators.page_info.has_next_page
+
+      collaborators_contributions.data.enterprise.owner_info.outside_collaborators.nodes.each do |collaborator|
+        user_tuple = OpenStruct.new
+        user_tuple.avatar_url                 = collaborator.avatar_url
+        user_tuple.created_at                 = collaborator.created_at
+        user_tuple.login                      = collaborator.login
+        user_tuple.name                       = collaborator.name
+        user_tuple.updated_at                 = collaborator.updated_at
+        user_tuple.has_contributions          = collaborator.contributions_collection.has_any_contributions
+        user_tuple.restricted_contributions   = collaborator.contributions_collection.restricted_contributions_count
+        user_tuple.commit_contributions       = collaborator.contributions_collection.total_commit_contributions
+        user_tuple.issue_contributions        = collaborator.contributions_collection.total_issue_contributions
+        user_tuple.pull_request_contributions = collaborator.contributions_collection.total_pull_request_contributions
+        user_tuple.member                     = false
+        all_users_contributions << user_tuple
+      end
+
+      sleep PAUSE
+    end
+
+    all_users_contributions.sort_by(&:login)
   end
 
   private
