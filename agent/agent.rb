25473954f9952f04d2ebdfe093f75088de791b6e
John Topley <john.topley@ons.gov.uk>
2022-11-28 16:24:47 +0000

    Updated agent application to retrieve the list of GitHub organisations then call the passed query for each one

--- agent/agent.rb
+++ agent/agent.rb
@@ -12,8 +12,6 @@ require_relative 'lib/github_error'
 # Class for executing named GitHub GraphQL API queries passed on the command-line.
 class Agent
   CONFIG = Configuration.new(ENV)
-  GITHUB = GitHub.new(CONFIG.github_enterprise, CONFIG.github_organisation,
-                      CONFIG.github_api_base_uri, CONFIG.github_token)
 
   def initialize
     raise 'Missing GraphQL query command-line argument' if ARGV.length.zero?
@@ -22,9 +20,15 @@ class Agent
 
     begin
       query = ARGV[0]
-      query_result = GITHUB.send(query)
-      firestore = Firestore.new(CONFIG.firestore_project)
-      firestore.save_document('github-explorer', query, query_result)
+
+      CONFIG.github_organisations.split(',').each do |organisation|
+        github = GitHub.new(CONFIG.github_enterprise, organisation,
+                            CONFIG.github_api_base_uri, CONFIG.github_token)
+
+        query_result = github.send(query)
+        firestore = Firestore.new(CONFIG.firestore_project)
+        firestore.save_document("github-explorer-#{organisation}", query, query_result)
+      end
     rescue GitHubError => e
       logger.error(%(A GitHub GraphQL API error occurred: #{e.message}\n#{e.backtrace.join("\n")}))
       exit(1)
