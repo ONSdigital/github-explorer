9e59eb9a2849d7504dbb153f6be82a220da551ef
John Topley <john.topley@ons.gov.uk>
2023-03-23 13:41:19 +0000

    Reverted 69c0bb4cd799471040ee3209346a7fa92f727299

--- agent/lib/github_error.rb
+++ agent/lib/github_error.rb
@@ -7,20 +7,8 @@ class GitHubError < StandardError
     @errors = errors
   end
 
-  def location_column
-    @errors.details['data'].first['locations'].first['column']
-  end
-
-  def location_line
-    @errors.details['data'].first['locations'].first['line']
-  end
-
   def message
     @errors.details['data'].first['message']
-  end
-
-  def path
-    @errors.details['data'].first['path'].join('/')
   end
 
   def type
