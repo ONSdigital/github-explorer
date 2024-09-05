658d33f5a9946afe1834369082391ce1f907f666
John Topley <john.topley@ons.gov.uk>
2024-09-05 14:42:00 +0100

    Added functions for displaying dates in local times

--- webapp/public/javascripts/app.js
+++ webapp/public/javascripts/app.js
@@ -1,4 +1,41 @@
 $(document).ready(function() {
+  displayDatesInLocalTime();
+
+  function displayDatesInLocalTime() {
+    const dateElements = document.querySelectorAll('.date');
+  
+    dateElements.forEach(element => {
+      const utcDateStr = element.textContent.trim();
+      const localDateStr = parseAndConvertToLocalTime(utcDateStr + ':00');
+      console.log(`localDateStr = ${localDateStr}`);
+  
+      element.textContent = localDateStr;
+    });
+  }
+
+  function parseAndConvertToLocalTime(utcDateString) {
+    if (utcDateString === "-") {
+      return "-";
+    }
+  
+    const [day, month, year, time] = utcDateString.split(' ');
+  
+    const months = {
+        Jan: '01', Feb: '02', Mar: '03', Apr: '04', May: '05', Jun: '06',
+        Jul: '07', Aug: '08', Sep: '09', Oct: '10', Nov: '11', Dec: '12'
+    };
+  
+    const formattedUTCDate = `${year}-${months[month]}-${day}T${time}Z`;
+    const dateObj = new Date(formattedUTCDate);
+    const datePart = dateObj.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' });
+    const timePart = dateObj.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: false });
+  
+    console.log(`datePart = ${datePart}`);
+    console.log(`timePart = ${timePart}`);
+    
+    return `${datePart} ${timePart}`;
+  }
+
   $("#selected-organisation").change(function() {
     Cookies.set('github-explorer-organisation', $(this).val());
     window.location.reload();
